720aa7887ccd32fdfefc6bbe29e52d45f4e508db
henok8515 <henokegezew44@gmail.com>
2023-11-16 23:59:01 +0300

    building the skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
+import React, { useState } from "react";
+import "./App.css";
+import InputField from "./components/InputField";
+import ListTodo from "./components/ListTodo";
+import { TodoModel } from "./components/model";
 function App() {
+  const [input, setInput] = useState<string>("");
+  const [todos, setTodos] = useState<TodoModel[]>([]);
+  const addTodo = (e: React.FormEvent) => {
+    e.preventDefault();
+    // const value = (e.target as HTMLInputElement).value;
+    if (input) {
+      setTodos([...todos, { todo: input, id: Date.now(), isDone: false }]);
+      console.log(todos, "todos");
+      console.log(input, "input");
+      setInput("");
+    }
+  };
   return (
-    <div className="text-red-500">
-      <p className="text-red-700">hello</p>
+    <div>
+      <InputField input={input} addTodo={addTodo} setInput={setInput} />
+      <ListTodo input={input} todos={todos} setTodos={setTodos} />
     </div>
   );
 }
