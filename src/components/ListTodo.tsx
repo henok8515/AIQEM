9c48e4055afff9b6a4d3951fbbe31bdeb253b6d2
henok8515 <henokegezew44@gmail.com>
2023-11-17 14:10:58 +0300

    applying list todo

--- src/components/ListTodo.tsx
+++ src/components/ListTodo.tsx
@@ -1,3 +1,23 @@
-export default function ListTodo() {
-  return <div>ListTodo</div>;
+import React from "react";
+import { TodoModel } from "./model";
+
+import Todo from "./Todo";
+interface Props {
+  todos: TodoModel[];
+  input: string;
+  setTodos: React.Dispatch<React.SetStateAction<TodoModel[]>>;
 }
+const ListTodo = ({ todos, setTodos }: Props) => {
+  return (
+    <div>
+      {todos
+        .slice()
+        .reverse()
+        .map((todo) => (
+          <Todo todo={todo} todos={todos} setTodos={setTodos} />
+        ))}
+    </div>
+  );
+};
+
+export default ListTodo;
