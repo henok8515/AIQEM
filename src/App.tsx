20aaa0edc0250859c3d78185626e861af39b650d
henok8515 <henokegezew44@gmail.com>
2023-11-17 23:20:43 +0300

    adding local storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import InputField from "./components/InputField";
 import ListTodo from "./components/ListTodo";
 import { TodoModel } from "./components/model";
 function App() {
+  // fetching  data from localStorage
+  const getLocalItemes = () => {
+    const list = localStorage.getItem("todos");
+    if (list) {
+      return JSON.parse(localStorage.getItem("todos") || "{}");
+    } else {
+      return [];
+    }
+  };
   const [input, setInput] = useState<string>("");
-  const [todos, setTodos] = useState<TodoModel[]>([]);
+  const [todos, setTodos] = useState<TodoModel[]>(getLocalItemes());
   const addTodo = (e: React.FormEvent) => {
     e.preventDefault();
     // const value = (e.target as HTMLInputElement).value;
     if (input) {
       setTodos([...todos, { todo: input, id: Date.now(), isDone: false }]);
-      console.log(todos, "todos");
-      console.log(input, "input");
       setInput("");
     }
   };
 
+  useEffect(() => {
+    //adding our state to local storage
+
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
   return (
     <div className="flex justify-center flex-col items-center mt-6">
+      <h1 className="font-serif text-5xl">Add your task</h1>
       <InputField input={input} addTodo={addTodo} setInput={setInput} />
-      <ListTodo input={input} todos={todos} setTodos={setTodos} />
+      <ListTodo todos={todos} setTodos={setTodos} />
     </div>
   );
 }
--- src/components/model.tsx
+++ src/components/model.tsx
@@ -2,4 +2,10 @@ export interface TodoModel {
   id: number;
   todo: string;
   isDone: boolean;
+  // getLocalItemes: () => void;
+}
+
+export interface IUser {
+  todo: string;
+  catagories: string;
 }
